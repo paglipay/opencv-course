e7669ef744da3839744fc1eccb15fd29c7bdfe7b
Paul Aglipay <paglipay@gmail.com>
2023-03-25 16:31:36 -0700

    ./Resources/Faces/train/Ben Afflek/2.jpg

--- Section #1 - Basics/faceRecognize-2.py
+++ Section #1 - Basics/faceRecognize-2.py
@@ -7,7 +7,7 @@ while True:
         # image=face_recognition.load_image_file('./Resources/Photos/group 2.jpg')
         fr_image=cv.cvtColor(image,cv.COLOR_BGR2RGB)
         face_locations=face_recognition.face_locations(fr_image)
-        print(face_locations)
+        # print(face_locations)
         if face_locations is not None:
             # print(f'Number of faces found = {len(face_locations)}')
             for (row1,col1,row2,col2) in face_locations:
